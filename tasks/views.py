8473a8e71de88d0611e94cb6d7acdc70136947d8
erik-nathan <eriknathan.contato@gmail.com>
2023-05-16 13:48:37 -0300

    feat: criando a edição e delete

--- tasks/views.py
+++ tasks/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
 from .forms import TaskForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -26,6 +27,31 @@ def newTask(request):
     else:
         form = TaskForm()
         return render(request, 'tasks/addTask.html', {'form': form})
+
+
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        task = TaskForm(request.POST, instance=task)
+
+        if form.is_valid():
+            task.save()
+            return redirect('/')
+        else:
+            return render(request, 'tasks/editTask.html', {'form': form, 'task': task})
+    else:
+        return render(request, 'tasks/editTask.html', {'form': form, 'task': task})
+
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+
+    messages.info(request, 'Tafera apagada com Sucesso!')
+
+    return redirect('/')
 
 
 def helloworld(request):
